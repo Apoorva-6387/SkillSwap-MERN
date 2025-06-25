34a4ccccee43fa0583b73ff0fae93f5d55497b00
Apoorva-6387 <apoorva6387@gmail.com>
2025-06-25 15:28:12 +0530

    Added auth middleware and protected /profile route with JWT

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,7 +7,8 @@ const registerUser = async (req, res) => {
 
   try {
     const userExist = await User.findOne({ email });
-    if (userExist) return res.status(400).json({ msg: "Email already in use" });
+    if (userExist)
+      return res.status(400).json({ msg: "Email already in use" });
 
     const hashedPass = await bcrypt.hash(password, 10);
 
@@ -28,24 +29,41 @@ const loginUser = async (req, res) => {
 
   try {
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ msg: "Invalid email or password" });
+    if (!user)
+      return res.status(400).json({ msg: "Invalid email or password" });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid email or password" });
+    if (!isMatch)
+      return res.status(400).json({ msg: "Invalid email or password" });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
 
     res.json({
       token,
       user: {
         id: user._id,
         name: user.name,
-        email: user.email
-      }
+        email: user.email,
+      },
     });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
-module.exports = { registerUser, loginUser };
+const getProfile = async (req, res) => {
+  try {
+    res.json(req.user); // comes from authMiddleware
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getProfile,
+};
+
